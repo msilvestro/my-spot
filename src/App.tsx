428aba73ddc4787a22ccc3b92a9aadcb1342dbb3
Matteo Silvestro <matteosilvestro@live.it>
2020-12-17 23:19:34 +0100

    Create basic episode buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
 
-const App = () => {
+const App: React.FC = () => {
+  const name = "Matteo"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sei seduto al mio posto</h1>
+      <p>Ciao {name}, cosa vuoi vedere oggi?</p>
+      <div id="episodeButtonsContainer">
+        {[
+          { title: "Sitcom", duration: "20 minuti" },
+          { title: "Puntata standard", duration: "40 minuti" },
+          { title: "Puntata lunga", duration: "60 minuti" },
+          { title: "Film", duration: "2 ore" },
+        ].map((episode) => (
+          <div
+            key={episode.title}
+            className="episodeButton"
+            onClick={() => console.log("episode")}
+          >
+            <div className="episodeTitle">
+              <span>{episode.title}</span>
+            </div>
+            <div className="episodeDuration">{episode.duration}</div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }

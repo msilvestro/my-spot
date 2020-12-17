9d03e23e379e7515356725a1a50f2e3ea66501f3
Matteo Silvestro <matteosilvestro@live.it>
2020-12-17 23:32:53 +0100

    Allow selecting different episode types

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css"
 
 const App: React.FC = () => {
   const name = "Matteo"
+  const [selectedEpisode, setSelectedEpisode] = useState(0)
+
+  const setActive = (baseClassName: string, condition: boolean) => {
+    return condition
+      ? `${baseClassName} ${baseClassName}-active`
+      : baseClassName
+  }
 
   return (
     <div className="App">
@@ -10,22 +17,26 @@ const App: React.FC = () => {
       <p>Ciao {name}, cosa vuoi vedere oggi?</p>
       <div id="episodeButtonsContainer">
         {[
-          { title: "Sitcom", duration: "20 minuti" },
-          { title: "Puntata standard", duration: "40 minuti" },
-          { title: "Puntata lunga", duration: "60 minuti" },
-          { title: "Film", duration: "2 ore" },
-        ].map((episode) => (
-          <div
-            key={episode.title}
-            className="episodeButton"
-            onClick={() => console.log("episode")}
-          >
-            <div className="episodeTitle">
-              <span>{episode.title}</span>
+          { title: "Sitcom", duration: 20 },
+          { title: "Puntata standard", duration: 40 },
+          { title: "Puntata lunga", duration: 60 },
+          { title: "Film", duration: 120 },
+        ].map((episode) => {
+          const selected = episode.duration === selectedEpisode
+
+          return (
+            <div
+              key={episode.title}
+              className={setActive("episodeButton", selected)}
+              onClick={() => setSelectedEpisode(episode.duration)}
+            >
+              <div className={setActive("episodeTitle", selected)}>
+                <span>{episode.title}</span>
+              </div>
+              <div className="episodeDuration">~ {episode.duration} minuti</div>
             </div>
-            <div className="episodeDuration">{episode.duration}</div>
-          </div>
-        ))}
+          )
+        })}
       </div>
     </div>
   )

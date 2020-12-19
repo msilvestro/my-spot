45fead43ea86cac89d8b65460b13c206b76a1c93
Matteo Silvestro <matteosilvestro@live.it>
2020-12-19 23:06:38 +0100

    Allow starting/stopping episode

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,40 @@ import "./App.css"
 
 import firebase from "./api/firebase"
 
-type User = {
+interface Dictionary<T> {
+  [key: string]: T
+}
+
+interface User {
   name: string
   watching: boolean
 }
 
 const App: React.FC = () => {
+  const myId = "matteo"
   const name = "Matteo"
-  const [users, setUsers] = useState<Array<User>>([])
+  const [users, setUsers] = useState<Dictionary<User>>({})
   const [selectedDuration, setSelectedDuration] = useState(0)
 
   const getUsers = async () => {
-    const response = await firebase.get("users.json")
-    setUsers(Object.values(response.data))
+    try {
+      const response = await firebase.get("users.json")
+      setUsers(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const updateWatching = async (watching: boolean) => {
+    try {
+      const response = await firebase.put(
+        `users/${myId}/watching.json`,
+        watching.toString()
+      )
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   useEffect(() => {
@@ -30,7 +51,7 @@ const App: React.FC = () => {
     <div className="App">
       <h1>Sei seduto al mio posto!</h1>
       <div id="tvs-container">
-        {users.map((user) => {
+        {Object.values(users).map((user) => {
           const isMe = name === user.name
           const watchingSentencte = isMe ? "stai guardando" : "sta guardando"
 
@@ -53,33 +74,50 @@ const App: React.FC = () => {
           )
         })}
       </div>
-      <p>Ciao {name}, cosa vuoi vedere oggi?</p>
-      <div id="episodes-container">
-        {[
-          { title: "Sitcom", duration: 20 },
-          { title: "Puntata standard", duration: 40 },
-          { title: "Puntata lunga", duration: 60 },
-          { title: "Film", duration: 120 },
-        ].map((episode) => {
-          const selected = episode.duration === selectedDuration
 
-          return (
-            <div
-              key={episode.title}
-              className={"episode" + toggleClass("episode-selected", selected)}
-              onClick={() => setSelectedDuration(episode.duration)}
+      {users[myId] && (
+        <div>
+          <p>Ciao {name}, cosa vuoi vedere oggi?</p>
+          <div id="episodes-container">
+            {[
+              { title: "Sitcom", duration: 20 },
+              { title: "Puntata standard", duration: 40 },
+              { title: "Puntata lunga", duration: 60 },
+              { title: "Film", duration: 120 },
+            ].map((episode) => {
+              const selected = episode.duration === selectedDuration
+
+              return (
+                <div
+                  key={episode.title}
+                  className={
+                    "episode" + toggleClass("episode-selected", selected)
+                  }
+                  onClick={() => setSelectedDuration(episode.duration)}
+                >
+                  <div className="title">
+                    <span>{episode.title}</span>
+                  </div>
+                  <div className="duration">~ {episode.duration} minuti</div>
+                </div>
+              )
+            })}
+          </div>
+          {!users[myId].watching ? (
+            <button
+              id="start"
+              disabled={selectedDuration === 0}
+              onClick={() => updateWatching(true)}
             >
-              <div className="title">
-                <span>{episode.title}</span>
-              </div>
-              <div className="duration">~ {episode.duration} minuti</div>
-            </div>
-          )
-        })}
-      </div>
-      <button id="start" disabled={selectedDuration === 0}>
-        Comincia a guardare
-      </button>
+              Comincia a guardare
+            </button>
+          ) : (
+            <button id="stop" onClick={() => updateWatching(false)}>
+              Interrompi
+            </button>
+          )}
+        </div>
+      )}
     </div>
   )
 }

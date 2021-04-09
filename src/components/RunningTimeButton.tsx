8babb58a35458e2305136e48bc6c8b88d44b7d51
Matteo Silvestro <matteosilvestro@live.it>
2021-04-09 12:07:55 +0200

    Add running time button component

---
+++ src/components/RunningTimeButton.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react"
+
+import { toggleClass } from "../utils/css"
+
+type Props = {
+  title: string
+  runningTime: number
+  selected: boolean
+  setSelected(): void
+}
+
+const RunningTimeButton: FC<Props> = ({
+  title,
+  runningTime,
+  selected,
+  setSelected,
+}: Props) => {
+  return (
+    <div
+      className={"episode" + toggleClass("episode-selected", selected)}
+      onClick={() => setSelected()}
+    >
+      <div className="title">
+        <span>{title}</span>
+      </div>
+      <div className="runningTime">~ {runningTime} minuti</div>
+    </div>
+  )
+}
+
+export default RunningTimeButton
